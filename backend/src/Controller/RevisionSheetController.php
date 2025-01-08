c0e6d1e3263d66873f807e78f2987cb311a2fb69
Badyss <badyss.blt@gmail.com>
2025-01-08 17:48:52 +0100

    feat: adding AI Groq for create a revision sheet

--- backend/src/Controller/RevisionSheetController.php
+++ backend/src/Controller/RevisionSheetController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\RevisionSheet;
+use Doctrine\ORM\EntityManagerInterface;
 use Smalot\PdfParser\Parser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class RevisionSheetController extends AbstractController
@@ -25,8 +28,9 @@ class RevisionSheetController extends AbstractController
         ]);
     }
 
+    #[IsGranted('ROLE_USER')]
     #[Route('/api/revision/upload', name: 'app_revision_sheet_upload', methods: ['POST'])]
-    public function uploadPdf(Request $request): Response
+    public function uploadPdf(Request $request, EntityManagerInterface $manager): Response
     {
         $uploadedFile = $request->files->get('file');
 
@@ -35,6 +39,22 @@ class RevisionSheetController extends AbstractController
 
             $text = $this->convertPdfToText($filePath);
 
+            $title = $request->getPayload()->get('title');
+
+            $revisionText = $this->generateRevisionSheetFromGroq($text);
+
+            $revision = new RevisionSheet();
+
+            $revision->setTitle($title);
+
+            $revision->setContent($text);
+
+            $revision->setOwner($this->getUser());
+
+            $manager->persist($revision);
+            $manager->flush();
+
+            return $this->json(['message' => 'La note a bien été créer', 'id' => $revision->getId()], Response::HTTP_CREATED);
         }
 
         return new Response('Aucun fichier valide fourni.', Response::HTTP_BAD_REQUEST);
@@ -46,6 +66,35 @@ class RevisionSheetController extends AbstractController
         $pdf = $parser->parseFile($filePath);
 
         return $pdf->getText();
+    }
+
+    private function generateRevisionSheetFromGroq(string $text): string
+    {
+        // URL de l'API Groq
+        $apiUrl = 'https://api.groq.com/openai/v1/chat/completions';
+
+
+        $payload = [
+            'model' => 'llama-3.3-70b-versatile',
+            'messages' => [
+                [
+                    'role' => 'user',
+                    'content' => "Voici le texte extrait du PDF :\n$text\n Crée une fiche de révision formatée en **gras** pour les points importants, avec des *italiques* pour les exemples et des retours à la ligne pour chaque section."
+                ]
+            ]
+        ];
+
+        $response = $this->httpClient->request('POST', $apiUrl, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $_ENV['GROQ_API'],
+            ],
+            'json' => $payload,
+        ]);
+
+        $responseData = $response->toArray();
+
+        return $responseData['choices'][0]['message']['content'] ?? 'Fiche de révision non disponible.';
     }
 
 
